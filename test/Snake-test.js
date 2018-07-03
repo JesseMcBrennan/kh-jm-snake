cbefb59d49b5aa8ff28b06013def6f81a58d9afa
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-03 17:39:21 -0600

    Add snake tests

---
+++ test/Snake-test.js
@@ -0,0 +1,9 @@
+const { assert } = require('chai');
+const Block = require('../lib/Block.js');
+
+describe('Snake', function {
+  it('should have properties', function() {
+    const snake = new Snake(50, 50, 20, 20, 'rgb(200, 0, 0)', 1)
+    
+  })
+})
