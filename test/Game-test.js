89c18e603107944adecfb1a9f1455bd8ed396590
Kent Huntsman <kenthuntsman@yahoo.com>
2018-07-04 16:07:51 -0600

    Add game-test.js testing

---
+++ test/Game-test.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+const Game = require('../lib/Game.js');
+const Snake = require('../lib/Snake.js')
+const Food = require('../lib/Food.js')
+const Block = require('../lib/Block.js')
+
+describe('Game',() => {
+  let game;
+  let snake;
+  let food;
+  beforeEach(() => {
+    game = new Game(false, false, 1, food = new Block(50, 50, 20, 20, '#000000'),snake = new Snake(50, 50, 20, 20, '#000000', 1),
+          'right', 0, [])
+  })
+  it('should exist',() => {
+    expect(game).to.exist;
+    expect(game).to.be.an.instanceOf(Game);
+
+  })
+  it('should set instance properties', () => {
+    let food = new Block(50 , 50, 20, 20, '#000000')
+
+
+    const actualState = {
+      gameOver: false,
+      isDead: false,
+      isGameOver: false,
+      lives: 1,
+      food: new Block(50, 50, 20, 20, '#000000'),
+      snake: new Snake(50, 50, 20, 20, '#000000',0),
+      direction: 'right',
+      score: 0,
+      parsedScores: [],
+    }
+    
+    const expectedState = {
+      gameOver: false,
+      isDead: false,
+      isGameOver: false,
+      lives: 1,
+      food: new Block(50, 50, 20, 20, '#000000'),
+      snake: new Snake(50, 50, 20, 20, '#000000',0),
+      direction: 'right',
+      score: 0,
+      parsedScores: [],
+    }
+
+    expect(actualState).to.deep.equal(expectedState);
+  })
+})
+
+
+
+
+
+
